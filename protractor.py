b5a883b5a1a272e8dc9e7ee5557fa472b53c202c
crumudgin <zac.chu@gmail.com>
2019-01-06 16:52:34 -0500

    added click detection

--- protractor.py
+++ protractor.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-# from matplotlib import pyplot as plt
+# import argparse
 
 
 def run_camera(camera_source):
@@ -11,7 +11,8 @@ def run_camera(camera_source):
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 		# cv2.imshow("frame", gray)
-		canny_edge_detection(gray)
+		edge_frame = canny_edge_detection(gray)
+		# cv2.namedWindow("edges")
 		if cv2.waitKey(1) & 0xFF == ord("q"):
 			break
 
@@ -21,5 +22,21 @@ def run_camera(camera_source):
 def canny_edge_detection(img):
 	edges = cv2.Canny(img, 100, 200)
 	cv2.imshow("frame", edges)
+	process_click.img = edges
+	cv2.setMouseCallback("frame", process_click)
+	return edges
 
-run_camera(1)
+# def isolate_object(img, x, y):
+	#make matrix of zeros in the same shape as the image
+	#starting at the x, y coordanites dijkstras out until you hit an edge
+	#the result will be the inner area of the edge
+
+def process_click(event, x, y, flags, params):
+	if event == cv2.EVENT_LBUTTONDOWN:
+		print("clickety")
+
+def main():
+	run_camera(1)
+
+if __name__ == '__main__':
+	main()
