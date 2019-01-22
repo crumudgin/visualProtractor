8f0e513f5699333d6f2711d46bde9d87df125b20
crumudgin <zac.chu@gmail.com>
2019-01-22 14:17:28 -0500

    cleaned up code

--- protractor.py
+++ protractor.py
@@ -4,89 +4,100 @@ import queue
 import imutils
 import math
 
+class Protractor:
 
-ACCEPED_VARIENCE = (50, 50, 50)
+	def __init__(self, accepted_varience, camera_source):
+		self.accepted_varience = accepted_varience
+		self.color_one = None
+		self.max_height = 0
+		self.camera_source = camera_source
+		self.current_img = None
+		# self.accepted_varience = (50, 50, 50)
 
-COLOR_ONE = None
+	@staticmethod
+	def preprocess_image(img):
+		img = imutils.resize(img, width=600)
+		img = cv2.GaussianBlur(img, (11, 11), 0)
+		img = cv2.GaussianBlur(img, (11, 11), 0)
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		return img
 
-MAX_HEIGHT = 0
+	@staticmethod
+	def morphology(img):
+		img = cv2.erode(img, None, iterations=2)
+		img = cv2.dilate(img, None, iterations=2)
+		img = cv2.erode(img, None, iterations=2)
+		img = cv2.dilate(img, None, iterations=2)
+		return img
 
-
-def run_camera(camera_source):
-	global MAX_HEIGHT
-	cap = cv2.VideoCapture(camera_source)
-	while(True):
-		ret, frame = cap.read()
-		cv2.imshow("frame", frame)
-		img = preprocess_image(frame)
-		if COLOR_ONE is not None:
-			hight = find_contours(mask_color(img), img)
-			cv2.imshow("tracking", img)
-			print(calc_angle(hight))
-			if cv2.waitKey(1) & 0xFF == ord("e"):
-				print("E!!!!!!!!!!!!!!!!!!!!!")
-				MAX_HEIGHT = hight
-
-
-		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		# edge_frame = canny_edge_detection(gray)
-		if cv2.waitKey(1) & 0xFF == ord("q"):
-			break
-
-	cap.release()
-	cv2.destroyAllWindows()
+	@staticmethod
+	def find_contours(mask, func):
+		contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		contours = imutils.grab_contours(contours)
+		return func(contours)
 
 
-def process_click(event, x, y, flags, params):
-	global COLOR_ONE
-	if event == cv2.EVENT_LBUTTONDOWN:
-		print(process_click.img[x, y])
-		COLOR_ONE = process_click.img[x, y]
+	def run_camera(self):
+		cap = cv2.VideoCapture(self.camera_source)
+		while(True):
+			ret, frame = cap.read()
+			cv2.imshow("frame", frame)
+			self.current_img = self.preprocess_image(frame)
+			cv2.setMouseCallback("frame", self.process_click)
+			if self.color_one is not None:
+				hight = self.find_contours(self.mask_color(self.morphology), self.draw_rect)
+				cv2.imshow("tracking", self.current_img)
+				print(self.calc_angle(hight))
+			key = cv2.waitKey(1) & 0xFF
+			if key == ord("q"):
+				break
+			elif key == ord("e"):
+					print("!!!! RESETING HIGHT VALUE !!!!")
+					try:
+						self.max_height = hight
+					except UnboundLocalError:
+						self.max_height = 0
 
-def preprocess_image(img):
-	img = imutils.resize(img, width=600)
-	img = cv2.GaussianBlur(img, (11, 11), 0)
-	img = cv2.GaussianBlur(img, (11, 11), 0)
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	process_click.img = img
-	cv2.setMouseCallback("frame", process_click)
-	return img
+		cap.release()
+		cv2.destroyAllWindows()
 
-def mask_color(img):
-	mask = cv2.inRange(img, COLOR_ONE - ACCEPED_VARIENCE, COLOR_ONE + ACCEPED_VARIENCE)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-	cv2.imshow("mask", mask)
-	return mask
 
-def find_contours(mask, img):
-	contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	contours = imutils.grab_contours(contours)
-	hight = 0
+	def process_click(self, event, x, y, flags, params):
+		if event == cv2.EVENT_LBUTTONDOWN:
+			self.color_one = self.current_img[x, y]
+		
 
-	if len(contours) > 0:
-		rect_contour = max(contours, key=cv2.contourArea)
-		rect = cv2.minAreaRect(rect_contour)
-		# print(rect)
-		box = cv2.boxPoints(rect)
-		x1, y1 = box[0]
-		x2, y2 = box[1]
-		hight = math.sqrt((x2 - x1) ** 2 + (y2 - y1) **2)
-		box = np.int0(box)
-		cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-	return hight
+	def mask_color(self, func):
+		mask = cv2.inRange(self.current_img, self.color_one - self.accepted_varience, self.color_one + self.accepted_varience)
+		mask = func(mask)
+		cv2.imshow("mask", mask)
+		return mask
+		
 
-def calc_angle(hight):
-	global MAX_HEIGHT
-	if hight >= MAX_HEIGHT:
-		MAX_HEIGHT = hight
-		return 90
-	return math.degrees(math.acos(hight / MAX_HEIGHT))
+	def draw_rect(self, contours):
+		hight = 1
+		if len(contours) > 0:
+			rect_contour = max(contours, key=cv2.contourArea)
+			rect = cv2.minAreaRect(rect_contour)
+			box = cv2.boxPoints(rect)
+			x1, y1 = box[0]
+			x2, y2 = box[1]
+			hight = math.sqrt((x2 - x1) ** 2 + (y2 - y1) **2)
+			box = np.int0(box)
+			cv2.drawContours(self.current_img, [box], 0, (0, 0, 255), 2)
+		return hight
+
+	def calc_angle(self, hight):
+		if self.max_height == 0:
+			self.max_height = hight
+		try:
+			return math.degrees(math.acos(hight / self.max_height))
+		except ValueError:
+			return -1
 
 def main():
-	run_camera(0)
+	protractor = Protractor((50, 50, 50), 1)
+	protractor.run_camera()
 
 if __name__ == '__main__':
 	main()
